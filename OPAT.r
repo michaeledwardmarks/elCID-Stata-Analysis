9e302707da3a1f05d274c5370d93a6ee7c1dd27e
David Miller <david@deadpansincerity.com>
2016-12-12 12:38:08 +0000

    UniqueN is only available in some versions of data.table

--- OPAT.r
+++ OPAT.r
@@ -8,6 +8,9 @@
 #install.packages("ggplot2")
 #install.packages("data.table")
 #install.packages("plyr")
+# 0.0.1 some versions of data.table don't have uniqueN
+#uniqueN <- function(x) length(attr(data.table:::forderv(x, retGrp=TRUE),"starts",TRUE))
+
 #### 0.1 Turn on the packages we need ####
 library(dplyr)
 library(data.table)
@@ -90,7 +93,7 @@ PID$year <- substr(PID$time1,1,4)
 PID$month <- substr(PID$time1,start = 6,7)
 PID <- arrange(PID,time1)
 PID$quarter <- as.numeric(PID$month)
-PID$month <- PID$time1 <- PID$time2 <- PID$created_by_id <- PID$updated_by_id <- NULL
+#PID$month <- PID$time1 <- PID$time2 <- PID$created_by_id <- PID$updated_by_id <- NULL
 PID$quarter <- recode(PID$quarter,"1:3=1;4:6=2;7:9=3;10:12=4")
 PID$reportingperiod <- paste(PID$year, PID$quarter, sep="_")
 
